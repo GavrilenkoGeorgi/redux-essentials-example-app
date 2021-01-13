c3af3260abc527c64cafbcdca5586a1cab4e337c
Gavrilenko <gavrilenko.georgi@gmail.com>
2021-01-13 13:42:45 +0200

    Update packages, setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	'extends': 'react-app',
+	'rules' : {
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'error',
+		'max-len': ['error', { 'code': 120 }],
+		'indent': [ 'error', 'tab' ],
+		'linebreak-style': [ 'error', 'unix' ],
+		'quotes': [ 'error', 'single' ],
+		'semi': [ 'error', 'never' ],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [ 'error', 'always' ],
+		'arrow-spacing': [ 'error', { 'before': true, 'after': true }
+		],
+	}
+}
