27ba4312e3054da7a35c2e05d3df97ae915ec0a5
Gavrilenko <gavrilenko.georgi@gmail.com>
2021-01-14 17:14:50 +0200

    Add performance optimizations
    Data used in components is normalized with the use of
    'createEntityAdapter' to reduce number component rerenders

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,12 +1,36 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { PostExcerpt } from './PostExcerpt'
-import { selectAllPosts, fetchPosts } from './postsSlice'
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
+import { Link } from 'react-router-dom'
+
+import {
+	fetchPosts,
+	selectPostIds,
+	selectPostById
+} from './postsSlice'
+
+
+let PostExcerpt = ({ postId }) => {
+	const post = useSelector(state => selectPostById(state, postId))
+
+	return <article className="post-excerpt" key={post.id}>
+		<h3>{post.title}</h3>
+		<PostAuthor userId={post.user} />
+		<TimeAgo timestamp={post.date} />
+		<p className="post-content">{post.content.substring(0, 100)}</p>
+		<Link to={`/posts/${post.id}`} className="button muted-button">
+			View Post
+		</Link>
+		<ReactionButtons post={post} />
+	</article>
+}
 
 export const PostsList = () => {
 	const dispatch = useDispatch()
-	const posts = useSelector(selectAllPosts)
+	const orderedPostIds = useSelector(selectPostIds)
 
 	const postStatus = useSelector(state => state.posts.status)
 	const error = useSelector(state => state.posts.error)
@@ -22,12 +46,8 @@ export const PostsList = () => {
 	if (postStatus === 'loading') {
 		content = <div className="loader">Loading...</div>
 	} else if (postStatus === 'succeeded') {
-		const orderedPosts = posts
-			.slice()
-			.sort((a, b) => b.date.localeCompare(a.date))
-
-		content = orderedPosts.map(post => (
-			<PostExcerpt key={post.id} post={post} />
+		content = orderedPostIds.map(postId => (
+			<PostExcerpt key={postId} postId={postId} />
 		))
 	} else if (postStatus === 'failed') {
 		content = <div>{error}</div>
